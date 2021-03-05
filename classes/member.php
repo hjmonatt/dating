575bb9b6eca65ee70d9660b48ad7d564e3e1254a
Heather Monatt <hmonatt@mail.greenriver.edu>
2021-03-04 21:49:35 -0500

    Added a classes folder and created Member class

---
+++ classes/member.php
@@ -0,0 +1,177 @@
+<?php
+
+class Member
+{
+    private $_fname;
+    private $_lname;
+    private $_age;
+    private $_gender;
+    private $_phone;
+    private $_email;
+    private $_state;
+    private $_seeking;
+    private $_bio;
+
+    /**
+     * Constructor - constructs a Member object
+     * @param $fname - String of member's first name
+     * @param $lname - String of member's last name
+     * @param $age - int of member's age
+     * @param $gender - String of member's gender
+     * @param $phone - String member's phone number
+     */
+    public function __construct($fname, $lname, $age, $gender, $phone)
+    {
+        $this->_fname = $fname;
+        $this->_lname = $lname;
+        $this->_age = $age;
+        $this->_gender = $gender;
+        $this->_phone = $phone;
+
+    }
+
+    /**
+     * @return String
+     */
+    public function getFName()
+    {
+        return $this->_fname;
+    }
+
+    /**
+     * @param String $fname
+     */
+    public function setFName($fname): void
+    {
+        $this->_fname = $fname;
+    }
+
+    /**
+     * @return String
+     */
+    public function getLName()
+    {
+        return $this->_lname;
+    }
+
+    /**
+     * @param String $lname
+     */
+    public function setLName($lname): void
+    {
+        $this->_lname = $lname;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAge()
+    {
+        return $this->_age;
+    }
+
+    /**
+     * @param int $age
+     */
+    public function setAge($age): void
+    {
+        $this->_age = $age;
+    }
+
+    /**
+     * @return String
+     */
+    public function getGender()
+    {
+        return $this->_gender;
+    }
+
+    /**
+     * @param String $gender
+     */
+    public function setGender($gender): void
+    {
+        $this->_gender = $gender;
+    }
+
+    /**
+     * @return String
+     */
+    public function getPhone()
+    {
+        return $this->_phone;
+    }
+
+    /**
+     * @param String $phone
+     */
+    public function setPhone($phone): void
+    {
+        $this->_phone = $phone;
+    }
+
+    /**
+     * @return String
+     */
+    public function getEmail()
+    {
+        return $this->_email;
+    }
+
+    /**
+     * @param String $email
+     */
+    public function setEmail($email): void
+    {
+        $this->_email = $email;
+    }
+
+    /**
+     * @return String
+     */
+    public function getState()
+    {
+        return $this->_state;
+    }
+
+    /**
+     * @param String $state
+     */
+    public function setState($state): void
+    {
+        $this->_state = $state;
+    }
+
+    /**
+     * @return String
+     */
+    public function getSeeking()
+    {
+        return $this->_seeking;
+    }
+
+    /**
+     * @param String $seeking
+     */
+    public function setSeeking($seeking): void
+    {
+        $this->_seeking = $seeking;
+    }
+
+    /**
+     * @return String
+     */
+    public function getBio()
+    {
+        return $this->_bio;
+    }
+
+    /**
+     * @param String $bio
+     */
+    public function setBio($bio): void
+    {
+        $this->_bio = $bio;
+    }
+
+}
